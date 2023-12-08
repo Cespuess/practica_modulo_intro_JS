e07e442097f9682429b0e52563af16dae7519773
Carles Espuñes <cespuess@gmail.com>
2023-12-08 09:45:54 +0100

    Ejercicio 1 terminado

---
+++ ejercicios/ejercicio1.js
@@ -0,0 +1,20 @@
+let usuario = {
+    name: "Carles",
+    surnames: "Espuñes Gonzalez",
+    bootcampModules: [
+        {name: "Node.js",
+        initialDate: "2024-02-12"
+        }, 
+        {name: "Git",
+        initialDate: "2023-11-28"
+        },
+        {name: "React",
+        initialDate: "2024-04-15"
+        }
+    ],
+    openToWork: true
+};
+
+
+console.log(usuario.bootcampModules[2].initialDate);
+
