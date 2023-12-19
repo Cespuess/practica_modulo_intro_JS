edd70368feefb9095aced7ab40da07c7d20a1561
Carles Espuñes <cespuess@gmail.com>
2023-12-19 21:09:45 +0100

    Ejercicio Wimblecode "normal" acabado con función point definida fuera y fichero index.js con simulación de partido

--- wimblecode/index.js
+++ wimblecode/index.js
@@ -1,13 +1,11 @@
 import { createMatch } from "./utils.js";
 
-// pruebas ----------------------------------------------------------------------------------------
-// Ejemplo de software
+// Simulación ----------------------------------------------------------------------------------------
+// Creación del partido
 const game = new createMatch('Alberto C', 'David J');
-// Cuando puntua el 1º judagor se registra de este modo
+// Empiezan los puntos
 game.pointWonBy(1);
-// Cuando puntua el 2º judagor se registra de este modo
 game.pointWonBy(2);
-// Quiero poder ver como va la ronda actual en todo momento
 console.log(game.getCurrentRoundScore()); // Alberto C 15-15 David J
 game.pointWonBy(1);
 console.log(game.getCurrentRoundScore()); // Alberto C 30-15 David J
@@ -15,84 +13,144 @@ game.pointWonBy(2);
 console.log(game.getCurrentRoundScore()); // Alberto C 30-30 David J
 game.pointWonBy(1);
 console.log(game.getCurrentRoundScore()); // Alberto C 40-30 David J
-game.pointWonBy(2);
-console.log(game.getCurrentRoundScore()); // Deuce
-// jugador 1 toma ventaja
-game.pointWonBy(1);
-console.log(game.getCurrentRoundScore()); // Advantage Alberto C
-// jugador 2 empata
-game.pointWonBy(2);
-console.log(game.getCurrentRoundScore()); // Deuce
-// jugador 2 toma ventaja
-game.pointWonBy(2);
-console.log(game.getCurrentRoundScore()); // Advantage David J
-// Con este punto jugador 2 gana la ronda
-game.pointWonBy(2);
-// Quiero poder ver como va la puntuación de un juego
-console.log(game.getRoundScore()); // Alberto C 0 David J 1
-// La primera ronda es para David le quedan 3 para ganar un juego
-game.pointWonBy(2); // Jugador 2 anota un punto
-game.pointWonBy(2); // Jugador 2 anota un punto
-game.pointWonBy(2); // Jugador 2 anota un punto
-game.pointWonBy(2); // Jugador 2 anota un punto
-console.log(game.getRoundScore());
-// David gana 2º ronda
-game.pointWonBy(1); // Player 2 wins the game
-game.pointWonBy(1); // Player 2 wins the game
-game.pointWonBy(1); // Player 2 wins the game
-game.pointWonBy(1); // Player 2 wins the game
-console.log(game.getRoundScore());
-game.pointWonBy(2); // Jugador 2 anota un punto
-game.pointWonBy(2); // Jugador 2 anota un punto
-game.pointWonBy(2); // Jugador 2 anota un punto
-game.pointWonBy(2); // Jugador 2 anota un punto
-console.log(game.getRoundScore());
-// David gana 3º ronda
-game.pointWonBy(1); // Player 2 wins the game
-game.pointWonBy(1); // Player 2 wins the game
-game.pointWonBy(1); // Player 2 wins the game
-game.pointWonBy(1); // Player 2 wins the game
-console.log(game.getRoundScore());
-// David gana 4º ronda
-game.pointWonBy(1); // Player 2 wins the game
-game.pointWonBy(1); // Player 2 wins the game
-game.pointWonBy(1); // Player 2 wins the game
-game.pointWonBy(1); // Player 2 wins the game
-console.log(game.getRoundScore());
-game.pointWonBy(2); // Player 2 wins the game
-game.pointWonBy(2); // Player 2 wins the game
-game.pointWonBy(2); // Player 2 wins the game
-game.pointWonBy(2); // Player 2 wins the game
-console.log(game.getRoundScore());
-game.pointWonBy(1); // Player 2 wins the game
-game.pointWonBy(1); // Player 2 wins the game
-game.pointWonBy(1); // Player 2 wins the game
-game.pointWonBy(1); // Player 2 wins the game
-console.log(game.getRoundScore());
-game.pointWonBy(2); // Player 2 wins the game
-game.pointWonBy(2); // Player 2 wins the game
-game.pointWonBy(2); // Player 2 wins the game
-game.pointWonBy(2); // Player 2 wins the game
-console.log(game.getRoundScore());
-game.pointWonBy(1); // Player 2 wins the game
-game.pointWonBy(1); // Player 2 wins the game
-game.pointWonBy(1); // Player 2 wins the game
-game.pointWonBy(1); // Player 2 wins the game
-console.log(game.getRoundScore());
-game.pointWonBy(2); // Player 2 wins the game
-game.pointWonBy(2); // Player 2 wins the game
-game.pointWonBy(2); // Player 2 wins the game
-game.pointWonBy(2); // Player 2 wins the game
-console.log(game.getRoundScore());
-game.pointWonBy(1); // Player 2 wins the game
-game.pointWonBy(1); // Player 2 wins the game
-game.pointWonBy(1); // Player 2 wins the game
-game.pointWonBy(1); // Player 2 wins the game
-console.log(game.getRoundScore());
-game.pointWonBy(2); // Player 2 wins the game
-game.pointWonBy(2); // Player 2 wins the game
-game.pointWonBy(2); // Player 2 wins the game
-game.pointWonBy(2); // Player 2 wins the game
-console.log(game.getRoundScore());
 
-console.log(game.getMatchScore())
+// Deuce
+game.pointWonBy(2);
+console.log(game.getCurrentRoundScore()); 
+
+// Ventaja Alberto C
+game.pointWonBy(1);
+console.log(game.getCurrentRoundScore()); 
+
+// Deuce
+game.pointWonBy(2);
+console.log(game.getCurrentRoundScore());
+
+// Ventaja David J
+game.pointWonBy(2);
+console.log(game.getCurrentRoundScore()); 
+
+// David J gana la primera ronda
+game.pointWonBy(2);
+console.log(game.getRoundScore()); // Alberto C 0 David J 1
+
+// David J gana la ronda
+game.pointWonBy(2); 
+game.pointWonBy(2); 
+game.pointWonBy(2); 
+game.pointWonBy(2); 
+console.log(game.getRoundScore()); // Alberto C 0 David J 2
+
+// Alberto C gana la ronda
+game.pointWonBy(1); 
+game.pointWonBy(1); 
+game.pointWonBy(1); 
+game.pointWonBy(1); 
+console.log(game.getRoundScore()); // Alberto C 1 David J 2
+
+// David J gana la ronda
+game.pointWonBy(2); 
+game.pointWonBy(2); 
+game.pointWonBy(2); 
+game.pointWonBy(2); 
+console.log(game.getRoundScore()); // Alberto C 1 David J 3
+
+// Alberto C gana la ronda
+game.pointWonBy(1); 
+game.pointWonBy(1); 
+game.pointWonBy(1); 
+game.pointWonBy(1); 
+console.log(game.getRoundScore()); // Alberto C 2 David J 3
+
+// Alberto C gana la ronda
+game.pointWonBy(1); 
+game.pointWonBy(1); 
+game.pointWonBy(1); 
+game.pointWonBy(1); 
+console.log(game.getRoundScore()); // Alberto C 3 David J 3
+
+// David J gana la ronda
+game.pointWonBy(2); 
+game.pointWonBy(2); 
+game.pointWonBy(2); 
+game.pointWonBy(2); 
+console.log(game.getRoundScore()); // Alberto C 3 David J 4
+
+// Alberto C gana la ronda
+game.pointWonBy(1); 
+game.pointWonBy(1); 
+game.pointWonBy(1); 
+game.pointWonBy(1); 
+console.log(game.getRoundScore()); // Alberto C 4 David J 4
+
+// David J gana la ronda
+game.pointWonBy(2); 
+game.pointWonBy(2); 
+game.pointWonBy(2); 
+game.pointWonBy(2); 
+console.log(game.getRoundScore()); // Alberto C 4 David J 5
+
+// Alberto C gana la ronda
+game.pointWonBy(1); 
+game.pointWonBy(1); 
+game.pointWonBy(1); 
+game.pointWonBy(1); 
+console.log(game.getRoundScore()); // Alberto C 5 David J 5
+
+// David J gana la ronda
+game.pointWonBy(2); 
+game.pointWonBy(2); 
+game.pointWonBy(2); 
+game.pointWonBy(2); 
+console.log(game.getRoundScore()); // Alberto C 5 David J 6
+
+// Alberto C gana la ronda
+game.pointWonBy(1); 
+game.pointWonBy(1); 
+game.pointWonBy(1); 
+game.pointWonBy(1); 
+console.log(game.getRoundScore()); // Alberto C 6 David J 6
+
+// David J gana la ronda y el game
+game.pointWonBy(2); 
+game.pointWonBy(2); 
+game.pointWonBy(2); 
+game.pointWonBy(2); 
+console.log(game.getRoundScore()); // Alberto C 0 David J 0, porque la ronda ya ha acabado y se ha sumado un game a David J
+
+console.log(game.getMatchScore()) // Alberto C 0 David J 1
+
+console.log(game.getWinner()); // devuelve null porque aún no hay ganador
+
+// David J gana todos los puntos hasta ganar el segundo game y el partido
+game.pointWonBy(2); 
+game.pointWonBy(2); 
+game.pointWonBy(2); 
+game.pointWonBy(2); 
+console.log(game.getRoundScore()); // Alberto C 0 David J 1
+
+game.pointWonBy(2); 
+game.pointWonBy(2); 
+game.pointWonBy(2); 
+game.pointWonBy(2); 
+console.log(game.getRoundScore()); // Alberto C 0 David J 2
+
+game.pointWonBy(2); 
+game.pointWonBy(2); 
+game.pointWonBy(2); 
+game.pointWonBy(2); 
+console.log(game.getRoundScore()); // Alberto C 0 David J 3
+
+game.pointWonBy(2); 
+game.pointWonBy(2); 
+game.pointWonBy(2); 
+game.pointWonBy(2); 
+console.log(game.getRoundScore()); // Alberto C 0 David J 4
+
+console.log(game.getMatchScore()); // Alberto C 0 David J 2
+
+console.log(game.getWinner()); // David J ha ganado el partido.
+
+// Una vez hay un ganador no se pueden sumar más puntos a ningún jugador
+game.pointWonBy(2); 
+
--- wimblecode/utils.js
+++ wimblecode/utils.js
@@ -21,56 +21,60 @@ export function createMatch(player1, player2) {
     this.advantage;
     this.winner;
 
-    this.pointWonBy = idPlayer => {
-        if (!this.winner){
-            if (!this.deuce) {
-                this.players[idPlayer - 1].currentRoundScore += 1;
-
-                if (this.players[idPlayer - 1].currentRoundScore === 4) { // cuando un jugador ha ganado la ronda sin llegar a deuce, ponemos el currentRoundScore a cero y le sumamos la ronda al ganador
-                    this.players[idPlayer - 1].roundScore +=1;
-                    this.players.forEach(player => player.currentRoundScore = 0);
-                }
-                else if (this.players[0].currentRoundScore === 3 && this.players[1].currentRoundScore === 3) { // comprobamos que los dos están a 40 para cambiar a deuce
-                    this.deuce = true;
-                }
-            } else { // si la ronda está en deuce
-                if (this.advantage === undefined) {
-                    this.advantage = idPlayer - 1;
-                }
-                else if (this.advantage === idPlayer - 1) { // nos dice quien ha ganado la ronda después de la ventaja y reiniciamos los valores de deuce y advantage
-                    this.players[idPlayer - 1].roundScore +=1;
-                    this.players.forEach(player => player.currentRoundScore = 0);
-                    this.deuce = false;
-                    this.advantage = undefined;
-                } else {
-                    this.advantage = undefined;
-                }
-            }
-            // controlamos el resultado de rondas para contabilizar los games
-            if (this.players[idPlayer - 1].roundScore === 7) { 
-                this.players[idPlayer - 1].gameScore += 1;
-                this.players.forEach(player => player.roundScore = 0);
-            } else if (this.players[idPlayer - 1].roundScore >= 4 && Math.abs(this.players[0].roundScore - this.players[1].roundScore) >= 2) {
-                this.players[idPlayer - 1].gameScore += 1;
-                this.players.forEach(player => player.roundScore = 0);
-            }
-            // si un jugador ha ganado dos games se proclama campeón
-            if (this.players[idPlayer - 1].gameScore === 2) {
-                this.winner = idPlayer;
-            }
-        }  else {
-            alert(`No se pueden jugar más rondas, ${this.players[this.winner - 1].name} ha ganado el partido.`)
-        } 
-    };
+    this.pointWonBy = point(this); 
     this.getCurrentRoundScore = () => { // devuelve el resultado de la ronda
         if (this.deuce && typeof this.advantage !== "number") return "Deuce";
         else if (this.deuce) return `Advantage ${this.players[this.advantage].name}`;
         else return `${this.players[0].name} ${this.points[this.players[0].currentRoundScore]} - ${this.points[this.players[1].currentRoundScore]} ${this.players[1].name}`
     };
-    this.getRoundScore = () => `${this.players[0].name} ${this.players[0].roundScore} - ${this.players[1].name} ${this.players[1].roundScore}`; // devuelve el resultado de las rondas ganadas
-    this.getMatchScore = () => `${this.players[0].name} ${this.players[0].gameScore} - ${this.players[1].name} ${this.players[1].gameScore}`; // devuelve el resultado de los games ganados
-    this.getWinner = () => (!this.winner ? null : `${this.players[this.winner - 1].name}`); // devuelve el nombre del ganador, o null si aún no hay vencedor
+    this.getRoundScore = () => `Rounds: ${this.players[0].name} ${this.players[0].roundScore} - ${this.players[1].name} ${this.players[1].roundScore}`; // devuelve el resultado de las rondas ganadas
+    this.getMatchScore = () => `Games: ${this.players[0].name} ${this.players[0].gameScore} - ${this.players[1].name} ${this.players[1].gameScore}`; // devuelve el resultado de los games ganados
+    this.getWinner = () => (!this.winner ? null : `${this.players[this.winner - 1].name} ha ganado el partido.`); // devuelve el nombre del ganador, o null si aún no hay vencedor
     
+}
+
+function point(partido) {  
+    return function (idPlayer) {  // suma el punto al jugador indicado
+        if (!partido.winner){
+            if (!partido.deuce) {
+                partido.players[idPlayer - 1].currentRoundScore += 1;
+
+                if (partido.players[idPlayer - 1].currentRoundScore === 4) { // cuando un jugador ha ganado la ronda sin llegar a deuce, ponemos el currentRoundScore a cero y le sumamos la ronda al ganador
+                    partido.players[idPlayer - 1].roundScore +=1;
+                    partido.players.forEach(player => player.currentRoundScore = 0);
+                }
+                else if (partido.players[0].currentRoundScore === 3 && partido.players[1].currentRoundScore === 3) { // comprobamos que los dos están a 40 para cambiar a deuce
+                    partido.deuce = true;
+                }
+            } else { // si la ronda está en deuce
+                if (partido.advantage === undefined) {
+                    partido.advantage = idPlayer - 1;
+                }
+                else if (partido.advantage === idPlayer - 1) { // nos dice quien ha ganado la ronda después de la ventaja y reiniciamos los valores de deuce y advantage
+                    partido.players[idPlayer - 1].roundScore +=1;
+                    partido.players.forEach(player => player.currentRoundScore = 0);
+                    partido.deuce = false;
+                    partido.advantage = undefined;
+                } else {
+                    partido.advantage = undefined;
+                }
+            }
+            // controlamos el resultado de rondas para contabilizar los games
+            if (partido.players[idPlayer - 1].roundScore === 7) { 
+                partido.players[idPlayer - 1].gameScore += 1;
+                partido.players.forEach(player => player.roundScore = 0);
+            } else if (partido.players[idPlayer - 1].roundScore >= 4 && Math.abs(partido.players[0].roundScore - partido.players[1].roundScore) >= 2) {
+                partido.players[idPlayer - 1].gameScore += 1;
+                partido.players.forEach(player => player.roundScore = 0);
+            }
+            // si un jugador ha ganado dos games se proclama campeón
+            if (partido.players[idPlayer - 1].gameScore === 2) {
+                partido.winner = idPlayer;
+            }
+        }  else {
+            alert(`No se pueden jugar más rondas, ${partido.players[partido.winner - 1].name} ha ganado el partido.`)
+        }
+    }
 }
 
 
