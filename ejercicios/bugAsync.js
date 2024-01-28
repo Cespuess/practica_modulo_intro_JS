9519375aac33194c4e1c504e7ea89fe5d80f354a
Carles Espuñes <cespuess@gmail.com>
2024-01-28 17:12:56 +0100

    Readme.md listo

--- ejercicios/bugAsync.js
+++ ejercicios/bugAsync.js
@@ -1,27 +1,19 @@
 // Este programa simula una llamada asincrónica para obtener un usuario
 
 function obtenerUsuario(id) {
-    let usuario;
-
-    setTimeout( () => {
-        if (id === 1) {
-            usuario = { id: 1, nombre: "John Doe"};
-        }
-        console.log(usuario);
-    }, 2000);
-    
-    return usuario;
+    return new Promise ((resolve) => {
+        let usuario;    
+        setTimeout( () => {
+            if (id === 1) {
+                resolve(usuario = { id: 1, nombre: "John Doe"});
+            }
+        }, 2000);
+    })
 }
 
-const usuario = obtenerUsuario(1);
+obtenerUsuario(1)
+    .then((response) => console.log(response))
  
-
 /*
-El problema es que la función setTimeout invoca la función una vez
-pasados los milisegundos indicados como parámetro, por eso 
-el console.log(usuario) nos da undefined, porque nos lo imprime antes de asignarle el valor 
-a la variable. Pero pasados los 2000 milisegundos a la variable usuario se le asigna correctamente
-el valor { id: 1, nombre: "John Doe"}
-Para que nos imprima el valor correcto en pantalla he movido el console.log(usuario)
-dentro de la función setTimeout().
+El problema era que no se capturaba la función asíncrona, y por eso siempre el console.log(usuario) devolvía undefined.
 */ 
---
+++ readme.md
@@ -0,0 +1,25 @@
+# Práctica del módulo Introducción a JS
+
+En esta práctica hemos puesto en obra todo lo apredido durante el módulo con algunos ejercicios básicos:
+
+### **ejercicio1.js:**
+
+Un pequeño ejercicio en el cual hemos de imprimir en pantalla en valor de una propiedad de un objeto.
+
+### **bug.js:**
+
+Tenemos que encontrar un pequeño bug en el código que hace devolver un valor incorrecto de una operación para calcular un promedio de una lista de números.
+
+### **bugAsync.js:**
+
+En este caso el bug se produce en un programa que simula una llamada asíncrona para obtener un usuario.
+
+### **catalogo_musical.js:**
+
+Implementamos un programa en el cual definimos diferentes métodos para la gestión de un catálogo musical.
+
+### **Wimblecode:**
+Por último desarrollamos un programa que simula un torneo con normas parecido al tenis, siguiendo unas pautas marcadas por el supuesto cliente. 
+
+
+⚠️ **En el archivo adjunto Intro_JS.pdf encontraréis la información completa sobre los ejercicios de la práctica.**
